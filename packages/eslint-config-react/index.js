d55d52c464dfd8fc16f856545ddcce6eb910306d
pchmn <pchmn.dev@gmail.com>
2023-01-02 14:28:23 +0100

    init project

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'judgeme-ts',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react']
+    }
+  ]
+};
