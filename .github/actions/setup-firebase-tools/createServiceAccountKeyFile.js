f876a67b6eec2ba68b1b3dfa0f9965063c8440f3
pchmn <pchmn.dev@gmail.com>
2023-01-04 21:31:35 +0100

    ci: test

--- .github/actions/setup-firebase-tools/createServiceAccountKeyFile.js
+++ .github/actions/setup-firebase-tools/createServiceAccountKeyFile.js
@@ -1,7 +1,7 @@
 const writeFileSync = require('fs').writeFileSync;
 
 function main() {
-  console.log(process.argv[2])
+  console.log(process.argv[2], JSON.parse(process.argv[2]), JSON.stringify(JSON.parse(process.argv[2])))
   writeFileSync('/tmp/serviceAccountKey.json', process.argv[2]);
 }
 
