ba2611715f47923084307ee0a756b1b598ab64d2
pchmn <pchmn.dev@gmail.com>
2023-01-23 19:53:59 +0100

    feat(functions): init sendMessage function

--- apps/functions/src/sendMessage/index.ts
+++ apps/functions/src/sendMessage/index.ts
@@ -1,8 +1,12 @@
-import { FunctionParams, FunctionValidation } from '@kavout/core';
+import 'dotenv/config';
+
+import { DeviceDocument, FunctionParams, FunctionValidation } from '@kavout/core';
+import Expo from 'expo-server-sdk';
 import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { CallableContext, HttpsError } from 'firebase-functions/v1/https';
 
+const expoClient = new Expo({ accessToken: process.env.EXPO_TOKEN });
 const db = getFirestore(initializeApp());
 
 export default async (data: FunctionParams['sendMessage'], context: CallableContext) => {
@@ -15,8 +19,36 @@ export default async (data: FunctionParams['sendMessage'], context: CallableCont
 
   const currentToken = context.auth?.token;
 
-  if (!currentToken) {
-    throw new HttpsError('unauthenticated', 'User must be authenticated');
+  // if (!currentToken) {
+  //   throw new HttpsError('unauthenticated', 'User must be authenticated');
+  // }
+
+  const { to } = data;
+
+  const recipientDevice = (await db.collection('devices').doc(to).get()).data() as DeviceDocument;
+  console.log('recipientDevice', recipientDevice);
+
+  const receiptIds = [];
+  for (const token of recipientDevice.expoTokens) {
+    if (!Expo.isExpoPushToken(token)) {
+      console.error(`Push token ${token} is not a valid Expo push token`);
+      continue;
+    }
+
+    const res = await expoClient.sendPushNotificationsAsync([
+      {
+        to: token,
+        title: 'Push notification title',
+        priority: 'high',
+        data: { withSome: 'data' },
+      },
+    ]);
+    console.log('res', res);
+    receiptIds.push(res.filter(({ status }) => status === 'ok').map((ticket: any) => ticket.id));
+  }
+
+  for (const receiptId of receiptIds) {
+    console.log('receiptId', await expoClient.getPushNotificationReceiptsAsync(receiptId));
   }
 
   return { success: true };
