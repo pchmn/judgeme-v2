604dd8efa0eb345394e94f0702cd45731eba7a97
pchmn <pchmn.dev@gmail.com>
2023-02-02 19:38:18 +0100

    build(functions): optimize functions

--- apps/functions/rollup.config.js
+++ apps/functions/rollup.config.js
@@ -1,25 +1,26 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import renameNodeModules from "rollup-plugin-rename-node-modules";
 import { createRequire } from 'module';
+
 const require = createRequire(import.meta.url);
 const pkg = require('./package.json');
 
 export default {
   input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    sourcemap: true,
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+  },
   external: [...Object.keys(pkg.dependencies).filter((key) => key !== '@kavout/core')],
   plugins: [
     typescript(),
     nodeResolve({
       extensions: ['.ts', '.js'],
     }),
+    renameNodeModules('external')
   ],
-  onwarn: () => {
-    return;
-  },
-  output: {
-    file: 'dist/index.js',
-    format: 'es',
-    sourcemap: true,
-  },
-  inlineDynamicImports: true,
 };
