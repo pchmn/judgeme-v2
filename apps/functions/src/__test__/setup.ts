1901afeca7ea862af4bc6d7a8dc43ad5122bb9cd
pchmn <pchmn.dev@gmail.com>
2023-02-05 19:18:26 +0100

    ci(functions): optimize workflow

--- apps/functions/src/__test__/setup.ts
+++ apps/functions/src/__test__/setup.ts
@@ -10,7 +10,7 @@ const testFunctions = firebaseFunctionsTest(
   {
     projectId: testProjectId,
   },
-  process.env.GOOGLE_APPLICATION_CREDENTIALS
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_TEST
 );
 
 export { functions, testFunctions, testProjectId };
