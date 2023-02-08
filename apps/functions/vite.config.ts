8d3b37570ecc537d876e8eb7b843e92e196591f7
pchmn <pchmn.dev@gmail.com>
2023-02-08 19:32:54 +0100

    test(functions): fix test conf

--- apps/functions/vite.config.ts
+++ apps/functions/vite.config.ts
@@ -1,12 +1,10 @@
 /// <reference types="vitest" />
 
-// Configure Vitest (https://vitest.dev/config/)
-
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
     environment: 'node',
-    setupFiles: '/src/__test__/setup.ts',
+    setupFiles: `${__dirname}/src/__test__/setup.ts`,
   },
 });
