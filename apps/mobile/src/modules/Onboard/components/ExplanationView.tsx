361f7852e75d0f8380acb81edf09c15f1058a082
pchmn <pchmn.dev@gmail.com>
2023-02-26 13:25:48 +0100

    refactor(mobile): remove unused code

--- apps/mobile/src/modules/Onboard/components/ExplanationView.tsx
+++ apps/mobile/src/modules/Onboard/components/ExplanationView.tsx
@@ -1,17 +1,9 @@
 import { useTranslation } from 'react-i18next';
 
-import { useIsFirstLaunch } from '../hooks';
 import { PageView } from './PageView';
 
 export function ExplanationView({ onNext }: { onNext: () => void }) {
   const { t } = useTranslation();
-
-  const { set: setIsFirstLaunch } = useIsFirstLaunch();
-
-  const handleNextPress = () => {
-    // setIsFirstLaunch(false);
-    onNext();
-  };
 
   return (
     <PageView
@@ -19,7 +11,7 @@ export function ExplanationView({ onNext }: { onNext: () => void }) {
       description={t('welcomeScreen.explalantionView.description')}
       buttonLabel={t('common.next')}
       imageSrc={require('./users-around-world.png')}
-      onPress={handleNextPress}
+      onPress={onNext}
     />
   );
 }
