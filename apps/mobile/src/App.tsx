694c84b3fd51f24529798d14b19ec326f2e76f4a
pchmn <pchmn.dev@gmail.com>
2023-03-17 15:04:18 +0100

    Revert "feat(mobile): test onboard with material3 theme"
    This reverts commit f1c9580f9a4948fa62790b28b8d2d9e34c697b18.

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -39,7 +39,7 @@ export default function App() {
   return (
     <NavigationContainer theme={navigationTheme} linking={linking}>
       <Stack.Navigator
-        initialRouteName={'Onboard'}
+        initialRouteName={isFirstLaunch ? 'Onboard' : 'Home'}
         screenOptions={{
           headerShown: false,
         }}
