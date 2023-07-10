e71df1c61df84149cb09c418370f5e092b8a3bbb
pchmn <pchmn.dev@gmail.com>
2023-07-10 14:45:21 +0200

    build(mobile): update expo sdk to v49

--- packages/react-native/src/core/storage/useSecureStorage.ts
+++ packages/react-native/src/core/storage/useSecureStorage.ts
@@ -1,5 +1,5 @@
 import { randomUUID } from 'expo-crypto';
-import { getGenericPassword, setGenericPassword } from 'react-native-keychain';
+import { getGenericPassword, setGenericPassword, STORAGE_TYPE, UserCredentials } from 'react-native-keychain';
 import { MMKV, useMMKVObject } from 'react-native-mmkv';
 
 let storage: MMKV;
@@ -8,13 +8,19 @@ const ENCRYPTION_KEY = 'MMKV_ENCRYPTION_KEY';
 const ENCRYPTION_SERVICE = 'MMKV_ENCRYPTION_SERVICE';
 
 async function getEncryptionKey() {
-  const existingCredentials = await getGenericPassword({
-    service: ENCRYPTION_SERVICE,
-  });
+  let existingCredentials: false | UserCredentials;
+  try {
+    existingCredentials = await getGenericPassword({
+      service: ENCRYPTION_SERVICE,
+    });
+  } catch (e) {
+    existingCredentials = false;
+  }
   if (!existingCredentials) {
     const password = randomUUID();
     await setGenericPassword(ENCRYPTION_KEY, password, {
       service: ENCRYPTION_SERVICE,
+      storage: STORAGE_TYPE.FB,
     });
     return password;
   }
